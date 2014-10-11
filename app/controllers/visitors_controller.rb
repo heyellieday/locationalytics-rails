8e76e623364a2953cd0c990f673395be8e382c27
Ellie Day <heyellieday@gmail.com>
2014-10-11 17:03:25 -0500

    Initial Setup of project, controller and models

---
+++ app/controllers/visitors_controller.rb
@@ -0,0 +1,43 @@
+class VisitorsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def index
+  	params.permit(:callback)
+  	if params[:callback]
+  		respond_to do |format|
+  			#format.html { render json: {stories: Story.all}}
+	  		format.js { render json: {stories: Visitor.paginate(page: params[:pagination], :per_page => 3)}, :callback => params[:callback] }
+	  	end
+  	else
+  		render json: {stories: Visitor.all}
+  	end
+  end
+
+  def show
+  	params.permit(:callback)
+  	if params[:callback]
+  		respond_to do |format|
+  			#format.html { render json: {stories: Story.all}}
+	  		format.js { render json: Visitor.find_by(id: params[:id]), :callback => params[:callback] }
+	  	end
+	else
+		render json: Visitor.all
+	end
+  end
+  def show
+  	params.permit(:callback)
+  	if params[:callback]
+  		respond_to do |format|
+  			#format.html { render json: {stories: Story.all}}
+	  		format.js { render json: Visitor.find_by(id: params[:id]), :callback => params[:callback] }
+	  	end
+	else
+		render json: Visitor.all
+	end
+  end
+  def update
+  	# This is where we would make the api call to the API which on initial time, 
+  	# would populate the database with the current data.
+  	# We will set a cron job using the whenever job to submit a patch request every X minutes
+  	# In addition, the client can possibliy submit a request to get the most up to date info
+  end
+end
